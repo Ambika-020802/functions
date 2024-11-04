fc3aca545ce5288caacf5e57cc43d6251130ffe5
Ambika-020802 <ambikarekandar2002@gmail.com>
2024-11-04 23:56:01 +0530

    Add files via upload

---
+++ day.js
@@ -0,0 +1,12 @@
+function Name(){
+    let a = "Ambika";
+    console.log(a);
+    }
+
+    function Surname(){
+        let a1= "Rekandar";
+        console.log(a1);
+        
+    }
+    Name()
+    Surname()
